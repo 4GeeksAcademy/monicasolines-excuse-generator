2239a805a2f61964304b03202cdd7bd24bdbd400
Monica Solines <160787107+monicasolines@users.noreply.github.com>
2024-09-05 13:11:23 +0000

    excuse generator con buton

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let indexWho = Math.floor(Math.random() * 4);
+  let indexAction = Math.floor(Math.random() * 4);
+  let indexWhat = Math.floor(Math.random() * 3);
+  let indexWhen = Math.floor(Math.random() * 4);
+
+  function reloadPage() {
+    event.preventDefault();
+    location.reload();
+  }
+
+  let html = `
+      <p id="excuse" class="display-5"> ${who[indexWho]} ${action[indexAction]} ${what[indexWhat]} ${when[indexWhen]} </p>
+      <button type="submit" class="btn btn-danger" id="reloadPage"> New Excuse </button>
+  `;
+
+  document.querySelector("#excuse").innerHTML = html;
+  document.querySelector("#reloadPage").addEventListener("click", reloadPage);
 };
